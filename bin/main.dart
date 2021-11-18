156027446717098b65e98b9eaec1913de667cae5
github_Anand0616 <anand.arceus@gmail.com>
2021-11-18 10:54:54 +0800

    11.18

--- bin/main.dart
+++ bin/main.dart
@@ -1,40 +1,54 @@
 import 'package:dart_application_1/binary_seaech_tree.dart';
+import 'package:dart_application_1/tree_node.dart';
+import 'package:dart_application_1/heap.dart';
 
 void main() {
-  final tree = BinarySearchTree<int>();
-  tree.insert(43);
-  tree.insert(10);
-  tree.insert(79);
-  tree.insert(90);
-  tree.insert(12);
-  tree.insert(54);
-  tree.insert(11);
-  tree.insert(9);
-  tree.insert(50);
-  print(tree.root);
-
-  print(tree.contains(79));
-  print(tree.contains(100));
-  print(tree.contains(90));
-
-  // final tree = BinaryTreeNode('A',
-  //     leftChild: BinaryTreeNode('D'),
-  //     rightChild: BinaryTreeNode(
-  //       'R',
-  //       leftChild: BinaryTreeNode('T'),
-  //       rightChild: BinaryTreeNode('FUN'),
-  //     ));
-  // print(tree);
-  // tree.traverseInOrder((value) => print(value));
-  // tree.traversePreOrder((value) => print(value));
-  // tree.traversePostOrder((value) => print(value));
-
-  // tree.checkIsInTree('D');
-  // tree.checkIsInTree('S');
-
-  // final tree = createTree();
-  // tree.traverseTree();
+  // final list = [10, 5, 7, 2, 1];
+  final heap = Heap();
+  heap.insert(10);
+  heap.insert(5);
+  heap.insert(7);
+  heap.insert(2);
+  heap.insert(1);
+  heap.insert(20);
+  print(heap);
+  heap.removeRoot();
+  print(heap);
 }
+
+// final tree = BinarySearchTree<int>();
+// tree.insert(43);
+// tree.insert(10);
+// tree.insert(79);
+// tree.insert(90);
+// tree.insert(12);
+// tree.insert(54);
+// tree.insert(11);
+// tree.insert(9);
+// tree.insert(50);
+// print(tree.root);
+
+// print(tree.contains(79));
+// print(tree.contains(100));
+// print(tree.contains(90));
+
+// final tree = BinaryTreeNode('A',
+//     leftChild: BinaryTreeNode('D'),
+//     rightChild: BinaryTreeNode(
+//       'R',
+//       leftChild: BinaryTreeNode('T'),
+//       rightChild: BinaryTreeNode('FUN'),
+//     ));
+// print(tree);
+// tree.traverseInOrder((value) => print(value));
+// tree.traversePreOrder((value) => print(value));
+// tree.traversePostOrder((value) => print(value));
+
+// tree.checkIsInTree('D');
+// tree.checkIsInTree('S');
+
+// final tree = createTree();
+// tree.traverseTree();
 
 // TreeNode<String> createTree<T>() {
 //   final drink = TreeNode('drink');
---
+++ lib/heap.dart
@@ -0,0 +1,45 @@
+class Heap<E extends Comparable> {
+  List<E> _list = []; //<E>
+
+  int _leftChild(int parentIndex) => 2 * parentIndex + 1;
+
+  int _rightChild(int parentIndex) => 2 * parentIndex + 2;
+
+  int _parent(int childIndex) => (childIndex - 1) ~/ 2;
+
+  void _swap(int firstIndex, int secondIndex) {
+    final save = _list[firstIndex];
+    _list[firstIndex] = _list[secondIndex];
+    _list[secondIndex] = save;
+  }
+
+  void insert(E value) {
+    _list.add(value);
+    _moveUp(_list.length - 1);
+  }
+
+  void _moveUp(int index) {
+    var childIndex = index;
+    var parentIndex = _parent(childIndex);
+    var childValue = _list[childIndex];
+    var parentValue = _list[parentIndex];
+
+    while (childValue.compareTo(parentValue) > 0) {
+      _swap(childIndex, parentIndex);
+      childIndex = parentIndex;
+      parentIndex = _parent(parentIndex);
+      parentValue = _list[parentIndex];
+      if (childIndex == 0) return;
+    }
+  }
+
+  E? removeRoot() {
+    // swap root and last value
+    int last = _list.length - 1;
+    _swap(_parent(0), _parent(last));
+    // move the root down
+  }
+
+  @override
+  String toString() => _list.toString();
+}
